88cfcc5485976ef19d8a1e0e804eb0c9445afcb9
maharonline <ha698174@gmail.com>
2023-07-20 10:35:06 +0500

    class 9

---
+++ class9.dart
@@ -0,0 +1,21 @@
+void main() {
+  // print(studentInfo(5, 8));
+  // studentinfo(fblink: "|Khan .com");
+
+  int age = 19;
+  String result = age >= 18 ? "Allow" : "not Allow";
+  print(result);
+}
+
+// studentInfo(int num1, int num2) => num1 + num2;     ///Arrow Function
+
+// studentinfo({String? fblink}) {
+// String? abc = fblink ?? "Facebook.com";
+// print(abc);
+// }
+// studentinfo({String? fblink}) {
+  // String abc = fblink!;
+  // print(abc);
+
+
+
