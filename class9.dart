3ef913b4af5eaa4931eda6eaf97c5b9ae3d03d54
maharonline <ha698174@gmail.com>
2023-07-20 21:53:32 +0500

    Class 9

--- class9.dart
+++ class9.dart
@@ -1,10 +1,20 @@
+import 'dart:io';
+
 void main() {
   // print(studentInfo(5, 8));
   // studentinfo(fblink: "|Khan .com");
 
-  int age = 19;
-  String result = age >= 18 ? "Allow" : "not Allow";
-  print(result);
+  //int age = 19;
+  // String result = age >= 18 ? "Allow" : "not Allow";     //Tendary Operator
+  //print(result);
+
+  print("Enter a number:");
+  String op = stdin.readLineSync()!;
+  if (op == 1) {
+    print({hamza()});
+  } else {
+    print({ahmad()});
+  }
 }
 
 // studentInfo(int num1, int num2) => num1 + num2;     ///Arrow Function
@@ -14,8 +24,13 @@ void main() {
 // print(abc);
 // }
 // studentinfo({String? fblink}) {
-  // String abc = fblink!;
-  // print(abc);
+// String abc = fblink!;
+// print(abc);
 
+hamza() {
+  print("I AM HAAmza Ahmad");
+}
 
-
+ahmad() {
+  print("I AM a Khan");
+}
